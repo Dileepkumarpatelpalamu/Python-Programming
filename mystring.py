47012469cdcd3f81bdeeab0172550e7ae0973614
Dileep Kumar Patel <78139205+Dileepkumarpatelpalamu@users.noreply.github.com>
2021-02-04 22:06:39 +0530

    Update mystring.py

--- mystring.py
+++ mystring.py
@@ -1,4 +1,4 @@
-mytext = """ I want to like the bindu to indu How is best girl to acknowledge to understand many language """
+mytext = """ I want to like Python to acknowledge to understand many language """
 data = mytext.lower()
 for item in data:
 	print(item,end="\t")
